ad03eb9a8f08853bb0f6f2254243801084f12592
Ashith V L <ashithvl@gmail.com>
2022-07-27 08:08:19 +0530

    5. Before Aspect

--- src/main/java/com/codeWithAshith/SpringBootBasics/aspect/BeforeAspect.java
+++ src/main/java/com/codeWithAshith/SpringBootBasics/aspect/BeforeAspect.java
@@ -11,10 +11,17 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class BeforeAspect {
 
-    //execution(* PACKAGE.*.*(..))
+    // execution(* PACKAGE.*.*(..))
 
+    // Join Point - Specific intersection
+    // Aspect - combination od join point & advice
+    // Weaving - process of implementing Aop around method calls
+    // Weaver - the framework which implements is called Weaver
+
+    // Join Point
     @Before("execution(* com.codeWithAshith.SpringBootBasics.business.*.*(..))")
     public void before(JoinPoint joinPoint) {
+        // Advice
         System.out.println("Before Intercept calls - " + joinPoint);
     }
 }
